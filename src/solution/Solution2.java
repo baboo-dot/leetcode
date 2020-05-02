8490f96632df3443e532d37fd9d514201222faef
Baboo <1143095390@qq.com>
2020-05-02 09:59:54 +0800

    2020/5/2

--- src/solution/Solution2.java
+++ src/solution/Solution2.java
@@ -12,11 +12,14 @@ class Solution2 {
         int index = 0;
         int res = 0;
         for (char ch : chars) {
+            // 如果set添加失败了，里面有重复元素
+            // 从字符串起始位置index开始remove到原有重复元素
             if (!set.add(ch)) {
                 res = Math.max(res, set.size());
                 while (chars[index] != ch) {
                     set.remove((Character) chars[index++]);
                 }
+                //remove完index就是下一个无重复子字符串起始位置
                 index++;
             }
         }
